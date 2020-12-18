818ab183b01947e675c4a58c8d8d2a3967000cf3
Reyoth <serialkiwi@gmail.com>
2020-12-18 13:49:13 +0100

    modif snake

--- python/snake.py
+++ python/snake.py
@@ -2,21 +2,16 @@ class Player:
     prenom="" 
     colorBlue =(0,0,255) # RGB  Red / Green / Blue
     colorRed = (255,0,0)
-
     #position du serpent
     x =0
     y =0
-
     #changement a faire sur la position du sperpent
     changeX= 0
     changeY= 0
-
     taille = 10 #taille du carre des anneaux du serpent
     longueur= 1 #nb de carre pour le serpent
     listeSnake=[] #liste qui comprend la position de chaque anneau
     speed = 10
-    #direction : gauche = 1 / droite = 2
-    direction = 0
 
     #pour cree notre serpent, il lui faut un nom et une position de depart X et Y
     def __init__(self, prenom,position):
